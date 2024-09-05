9ab4b4ffeed0389602499dcd1a02a5b5486d7e4a
JERF21 <eduardo21reyes@gmail.com>
2024-09-05 13:36:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+window.addEventListener('load', () => {
+  let lon
+  let lat
+  let temperaturaValor = document.getElementById('temperatura-valor')
+  let temperaturaDescripcion = document.getElementById('temperatura-descripcion')
+  let ubicacion = document.getElementById('ubicacion')
+  let iconoAnimado = document.getElementById('icono-animado')
+  let vientoVelocidad = document.getElementById('viento-velocidad')
+
+  let btn = document.getElementById('Boton')
+  btn.addEventListener('click', () => {
+      let lugar = document.getElementById('lugar').value
+
+      if (lugar === "") {
+          alert("Por favor, ingresa una ciudad.");
+          return;
+      }
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${lugar}&lang=es&units=metric&appid=aa585bda70eb775150104f89a218970e`
+      fetch(url)
+        .then(response => { return response.json() })
+        .then(data => {
+          let temp = Math.round(data.main.temp)
+          temperaturaValor.textContent = `${temp} ° C`
+          let desc = data.weather[0].description
+          temperaturaDescripcion.textContent = desc.toUpperCase()
+          ubicacion.textContent = data.name
+          vientoVelocidad.textContent = `${data.wind.speed} m/s`
+          console.log(data.weather[0].main)
+          switch (data.weather[0].main) {
+            case 'Thunderstorm':
+              iconoAnimado.src = 'animated/thunder.svg'
+              console.log('TORMENTA');
+              break;
+            case 'Drizzle':
+              iconoAnimado.src = 'animated/rainy-2.svg'
+              console.log('LLOVIZNA');
+              break;
+            case 'Rain':
+              iconoAnimado.src = 'animated/rainy-7.svg'
+              console.log('LLUVIA');
+              break;
+            case 'Snow':
+              iconoAnimado.src = 'animated/snowy-6.svg'
+              console.log('NIEVE');
+              break;
+            case 'Clear':
+              iconoAnimado.src = 'animated/day.svg'
+              console.log('LIMPIO');
+              break;
+            case 'Atmosphere':
+              iconoAnimado.src = 'animated/weather.svg'
+              console.log('ATMOSFERA');
+              break;
+            case 'Clouds':
+              iconoAnimado.src = 'animated/cloudy-day-1.svg'
+              console.log('NUBES');
+              break;
+            default:
+              iconoAnimado.src = 'animated/cloudy-day-1.svg'
+              console.log('por defecto');
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+  }
+)
